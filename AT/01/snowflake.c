6a8bdac775ffeaf3d306778b9bd082b37e6cc305
josh-reynolds <josh.reynolds@frontier.com>
2025-08-12 14:14:53 -0700

    Adding are_identical() function and running some test cases

--- AT/01/snowflake.c
+++ AT/01/snowflake.c
@@ -26,6 +26,19 @@ int identical_left(int snow1[], int snow2[], int start){
 	return 1;
 }
 
+int are_identical(int snow1[], int snow2[]){
+	int start;
+	for (start = 0; start < 6; start++){
+		if (identical_right(snow1, snow2, start)){
+			return 1;
+		}
+		if (identical_left(snow1, snow2, start)){
+			return 1;
+		}
+	}
+	return 0;
+}
+
 void identify_identical(int values[], int n){
 	int i, j;
 	for (i = 0; i < n; i++){
@@ -40,9 +53,52 @@ void identify_identical(int values[], int n){
 }
 
 int main(void){
-	int a[5] = {1, 2, 3, 4, 5};
-	identify_identical(a, 5);
-	int b[5] = {1, 2, 1, 4, 5};
-	identify_identical(b, 5);
+	int a[6] = {1, 2, 3, 4, 5, 6};
+	int b[6] = {1, 2, 1, 4, 5, 6};
+	int c[6] = {4, 5, 6, 1, 2, 3};
+	int d[6] = {6, 5, 4, 3, 2, 1};
+
+	int ab = are_identical(a,b);
+	int ac = are_identical(a,c);
+	int ad = are_identical(a,d);
+	int bc = are_identical(b,c);
+	int bd = are_identical(b,d);
+	int cd = are_identical(c,d);
+
+	if (ab){
+		printf("A-B are identical.\n");
+	} else {
+		printf("A-B are NOT identical.\n");
+	}
+	
+	if (ac){
+		printf("A-C are identical.\n");
+	} else {
+		printf("A-C are NOT identical.\n");
+	}
+
+	if (ad){
+		printf("A-D are identical.\n");
+	} else {
+		printf("A-D are NOT identical.\n");
+	}
+
+	if (bc){
+		printf("B-C are identical.\n");
+	} else {
+		printf("B-C are NOT identical.\n");
+	}
+	
+	if (bd){
+		printf("B-D are identical.\n");
+	} else {
+		printf("B-D are NOT identical.\n");
+	}
+
+	if (cd){
+		printf("C-D are identical.\n");
+	} else {
+		printf("C-D are NOT identical.\n");
+	}
 	return 0;
 }

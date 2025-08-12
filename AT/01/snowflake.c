6aec7e534d9a83623e6fc30a909e26a836d79592
josh-reynolds <josh.reynolds@frontier.com>
2025-08-12 14:30:05 -0700

    Modifying identify_identical() to work with array of snowflakes, and taking input from STDIN

--- AT/01/snowflake.c
+++ AT/01/snowflake.c
@@ -2,6 +2,8 @@
 #include <stdlib.h>
 #include <string.h>
 
+#define SIZE 100000
+
 int identical_right(int snow1[], int snow2[], int start){
 	int offset;
 	for (offset = 0; offset < 6; offset++){
@@ -39,66 +41,28 @@ int are_identical(int snow1[], int snow2[]){
 	return 0;
 }
 
-void identify_identical(int values[], int n){
+void identify_identical(int snowflakes[][6], int n){
 	int i, j;
 	for (i = 0; i < n; i++){
 		for (j = i+1; j < n; j++){
-			if (values[i] == values[j]){
-				printf("Twin integers found.\n");
+			if (are_identical(snowflakes[i], snowflakes[j])){
+				printf("Twin snowflakes found.\n");
 				return;
 			}
 		}
 	}
-	printf("No two integers are alike.\n");
+	printf("No two snowflakes are alike.\n");
 }
 
 int main(void){
-	int a[6] = {1, 2, 3, 4, 5, 6};
-	int b[6] = {1, 2, 1, 4, 5, 6};
-	int c[6] = {4, 5, 6, 1, 2, 3};
-	int d[6] = {6, 5, 4, 3, 2, 1};
-
-	int ab = are_identical(a,b);
-	int ac = are_identical(a,c);
-	int ad = are_identical(a,d);
-	int bc = are_identical(b,c);
-	int bd = are_identical(b,d);
-	int cd = are_identical(c,d);
-
-	if (ab){
-		printf("A-B are identical.\n");
-	} else {
-		printf("A-B are NOT identical.\n");
+	static int snowflakes[SIZE][6];
+	int n, i, j;
+	scanf("%d", &n);
+	for (i = 0; i < n; i++){
+		for (j = 0; j < 6; j++){
+			scanf("%d", &snowflakes[i][j]);
+		}
 	}
-	
-	if (ac){
-		printf("A-C are identical.\n");
-	} else {
-		printf("A-C are NOT identical.\n");
-	}
-
-	if (ad){
-		printf("A-D are identical.\n");
-	} else {
-		printf("A-D are NOT identical.\n");
-	}
-
-	if (bc){
-		printf("B-C are identical.\n");
-	} else {
-		printf("B-C are NOT identical.\n");
-	}
-	
-	if (bd){
-		printf("B-D are identical.\n");
-	} else {
-		printf("B-D are NOT identical.\n");
-	}
-
-	if (cd){
-		printf("C-D are identical.\n");
-	} else {
-		printf("C-D are NOT identical.\n");
-	}
+	identify_identical(snowflakes, n);
 	return 0;
 }

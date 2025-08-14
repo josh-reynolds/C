857b6a07e173e55c8d8df1ee45cb204079783e8e
josh-reynolds <josh.reynolds@frontier.com>
2025-08-14 14:35:58 -0700

    Adding new_house() function

--- AT/02/halloween.c
+++ AT/02/halloween.c
@@ -7,7 +7,26 @@ typedef struct node {
 	struct node *left, *right;
 } node;
 
+node *new_house(int candy){
+	node *house = malloc(sizeof(node));
+	if (house == NULL){
+		fprintf(stderr, "malloc error\n");
+		exit(1);
+	}
+	house->candy = candy;
+	house->left = NULL;
+	house->right = NULL;
+	return house;
+}
+
 int main(void){
+	node *four = new_house(4);
+	node *nine = new_house(9);
+
+	node *B = malloc(sizeof(node));
+	B->left = four;
+	B->right = nine;
+
 	return 0;
 }
 

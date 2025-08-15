da660ee624a5bd1cb25d0f0761d73ce2abaa5a3f
josh-reynolds <josh.reynolds@frontier.com>
2025-08-15 14:33:45 -0700

    Adding a rudimentary stack implementation

--- AT/02/halloween.c
+++ AT/02/halloween.c
@@ -2,10 +2,13 @@
 #include <stdlib.h>
 #include <string.h>
 
+#define SIZE 255
+
 typedef struct node {
 	int candy;
 	struct node *left, *right;
 } node;
+
 
 node *new_house(int candy){
 	node *house = malloc(sizeof(node));
@@ -28,6 +31,40 @@ node *new_nonhouse(node *left, node *right){
 	nonhouse->left = left;
 	nonhouse->right = right;
 	return nonhouse;
+}
+
+int tree_candy(node *tree){
+
+}
+
+typedef struct stack {
+	node * values[SIZE];
+	int highest_used;
+} stack;
+
+stack *new_stack(void){
+	stack *s = malloc(sizeof(stack));
+	if (s == NULL){
+		fprintf(stderr, "malloc error\n");
+		exit(1);
+	}
+	s->highest_used = -1;
+	return s;
+}
+
+void push_stack(stack *s, node *value){
+	s->highest_used++;
+	s->values[s->highest_used] = value;
+}
+
+node * pop_stack(stack *s){
+	node * ret = s->values[s->highest_used];
+	s->highest_used--;
+	return ret;
+}
+
+int is_empty_stack(stack *s){
+	return s->highest_used == -1;
 }
 
 int main(void){

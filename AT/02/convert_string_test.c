d5411a3f9028180abf2a0c5f7bc152aa880411d6
josh-reynolds <josh.reynolds@frontier.com>
2025-08-16 14:50:42 -0700

    Adjusting to allow for two-digit numbers

--- AT/02/convert_string_test.c
+++ AT/02/convert_string_test.c
@@ -6,9 +6,11 @@
 
 int main(void){
 	char line[SIZE + 1];
-	int candy;
+	int candy, digit1, digit2;
 	gets(line);    // gets() should not be used...
-	candy = line[0] - '0';
+	digit1 = line[0] - '0';
+        digit2 = line[1] - '0';
+	candy = 10 * digit1 + digit2;
 	printf("%d\n", candy);
 	return 0;
 }

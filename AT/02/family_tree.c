f00e4eb57e98ee60204bcfb6504916e26af0563e
josh-reynolds <josh.reynolds@frontier.com>
2025-08-17 14:48:50 -0700

    Starting setup for family tree problem

---
+++ AT/02/family_tree.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+typedef struct node {
+	char *name;
+	int num_children;
+	struct node **children;
+	int score;
+} node;
+
+int main(void){
+	return 0;
+}
+

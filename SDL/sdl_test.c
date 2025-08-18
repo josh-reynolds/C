226de0cb548fe1cb559f50e139f21d1ce9b8e715
josh-reynolds <josh.reynolds@frontier.com>
2025-08-18 16:04:25 -0700

    Documenting SDL setup more clearly

--- SDL/sdl_test.c
+++ SDL/sdl_test.c
@@ -1,7 +1,18 @@
-// nix-shell -p SDL
-// drops into /nix/store/<UNIQUE>/
-// pulled version 1.2.15
-// to compile, need -lSDL flag:
+// Figuring out how to work with SDL on nixOS.
+// Validation code below from 'Focus on SDL' (Ernest Pazera), page 14.
+// Needed to modify the include slightly to find SDL.h.
+// That book is 22 years old, so things have changed, but I'll
+// start there. Code below compiles and reports successful 
+// initialization so that's something. https://search.nixos.org shows
+// SDL2 and SDL3 are available. We'll try '1' to match the book 
+// text for now.
+// 
+// To install SDL:
+//     nix-shell -p SDL
+//
+// Libraries will be found in /nix/store/<GUID>-SDL-1.2.15-dev/
+//
+// To compile, need -lSDL flag:
 //    gcc ./sdl_test.c -o sdl.out -lSDL
 
 #include <SDL/SDL.h>

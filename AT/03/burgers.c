833a8d69c7c590124765dbbb490b85c830747926
josh-reynolds <josh.reynolds@frontier.com>
2025-08-19 13:48:44 -0700

    Adding max() and solve_t() functions

--- AT/03/burgers.c
+++ AT/03/burgers.c
@@ -2,6 +2,39 @@
 #include <stdlib.h>
 #include <string.h>
 
+int max(int v1, int v2){
+	if (v1 > v2){
+		return v1;
+	} else {
+		return v2;
+	}
+}
+
+int solve_t(int m, int n, int t){
+	int first, second;
+	if (t == 0){
+		return 0;
+	}
+
+	if (t >= m){
+		first = solve_t(m, n, t - m);
+	} else {
+		first = -1;
+	}
+
+	if (t >= n){
+		second = solve_t(m, n, t - n);
+	} else {
+		second = -1;
+	}
+
+	if (first == -1 && second == -1){
+		return -1;
+	} else {
+		return max(first, second) + 1;
+	}
+}
+
 int main(void){
 	return 0;
 }

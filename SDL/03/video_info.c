e5b2e4bd6ec0dd1fc6c0456533feba35be7e34dc
josh-reynolds <josh.reynolds@frontier.com>
2025-08-19 13:14:49 -0700

    Video info program from Chapter 3

---
+++ SDL/03/video_info.c
@@ -0,0 +1,30 @@
+// From 'Focus on SDL' pp. 44-5
+#include <SDL/SDL.h>
+#include <stdlib.h>
+
+const SDL_VideoInfo* g_pVideoInfo = NULL;
+
+int main(int argc, char* argv[]){
+	if (SDL_Init(SDL_INIT_VIDEO) == -1){
+		fprintf(stderr, "Could not initialize SDL!\n");
+		exit(1);
+	} else {
+		fprintf(stdout, "SDL initialized properly!\n");
+		atexit(SDL_Quit);
+	}
+
+	g_pVideoInfo = SDL_GetVideoInfo();
+	fprintf(stdout, "\nVideo Information:\n");
+	fprintf(stdout, "hw_available? %d\n", g_pVideoInfo->hw_available);
+	fprintf(stdout, "wm_available? %d\n", g_pVideoInfo->wm_available);
+	fprintf(stdout, "blit_hw? %d\n", g_pVideoInfo->blit_hw);
+	fprintf(stdout, "blit_hw_CC? %d\n", g_pVideoInfo->blit_hw_CC);
+	fprintf(stdout, "blit_hw_A? %d\n", g_pVideoInfo->blit_hw_A);
+	fprintf(stdout, "blit_sw? %d\n", g_pVideoInfo->blit_sw);
+	fprintf(stdout, "blit_sw_CC? %d\n", g_pVideoInfo->blit_sw_CC);
+	fprintf(stdout, "blit_sw_A? %d\n", g_pVideoInfo->blit_sw_A);
+	fprintf(stdout, "blit_fill? %d\n", g_pVideoInfo->blit_fill);
+	fprintf(stdout, "video memory (in K)? %d\n", g_pVideoInfo->video_mem);
+	fprintf(stdout, "bits per pixel? %d\n", g_pVideoInfo->vfmt->BitsPerPixel);
+	return 0;
+}

e1aa3e0aa53093ed8e97453cc3a5132a40a6e3ad
josh-reynolds <josh.reynolds@frontier.com>
2025-08-20 15:12:23 -0700

    Adding min() and solve_k() functions, and test case data

--- AT/03/moneygrubbers.c
+++ AT/03/moneygrubbers.c
@@ -2,6 +2,35 @@
 #include <stdlib.h>
 #include <string.h>
 
+double min(double v1, double v2){
+	if (v1 < v2){
+		return v1;
+	} else {
+		return v2;
+	}
+}
+
+double solve_k(int num[], double price[], int num_schemes,
+		double unit_price, int num_items){
+	double best, result;
+	int i;
+	if (num_items == 0){
+		return 0;
+	} else {
+		result = solve_k(num, price, num_schemes, unit_price,
+				num_items - 1);
+		best = result + unit_price;
+		for (i = 0; i < num_schemes; i++){
+			if (num_items - num[i] >= 0){
+				result = solve_k(num, price, num_schemes,
+						unit_price, num_items - num[i]);
+				best = min(best, result + price[i]);
+			}
+		}
+		return best;
+	}
+}
+
 int main(void){
 	return 0;
 }

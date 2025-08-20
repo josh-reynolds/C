06e423b93750892a5302ecfcf22d8c6270cc3cf7
josh-reynolds <josh.reynolds@frontier.com>
2025-08-20 14:38:14 -0700

    Memoizing recursive Homer burger algorithm

--- AT/03/burgers.c
+++ AT/03/burgers.c
@@ -2,6 +2,8 @@
 #include <stdlib.h>
 #include <string.h>
 
+#define SIZE 10000
+
 unsigned long long total_calls;
 
 int max(int v1, int v2){
@@ -12,44 +14,59 @@ int max(int v1, int v2){
 	}
 }
 
-int solve_t(int m, int n, int t){
+int solve_t(int m, int n, int t, int memo[]){
 	int first, second;
 	total_calls++;
+
+	if (memo[t] != -2){
+		return memo[t];
+	}
+
 	if (t == 0){
-		return 0;
+		memo[t] = 0;
+		return memo[t];
 	}
 
 	if (t >= m){
-		first = solve_t(m, n, t - m);
+		first = solve_t(m, n, t - m, memo);
 	} else {
 		first = -1;
 	}
 
 	if (t >= n){
-		second = solve_t(m, n, t - n);
+		second = solve_t(m, n, t - n, memo);
 	} else {
 		second = -1;
 	}
 
 	if (first == -1 && second == -1){
-		return -1;
+		memo[t] = -1;
+		return memo[t];
 	} else {
-		return max(first, second) + 1;
+		memo[t] = max(first, second) + 1;
+		return memo[t];
 	}
 }
 
 void solve(int m, int n, int t){
 	int result, i;
+	int memo[SIZE];
+
+	for (i = 0; i <= t; i++){
+		memo[i] = -2;
+	}
+
 	total_calls = 0;
-	result = solve_t(m, n, t);
+	result = solve_t(m, n, t, memo);
+
 	if (result >= 0){
 		printf("%d\n", result);
 	} else {
 		i = t - 1;
-		result = solve_t(m, n, i);
+		result = solve_t(m, n, i, memo);
 		while (result == -1){
 			i--;
-			result = solve_t(m, n, i);
+			result = solve_t(m, n, i, memo);
 		}
 		printf("%d %d\n", result, t - i);
 	}

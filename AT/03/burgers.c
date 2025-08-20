879744bb01bb55f6f5a8518344659958fdbd6664
josh-reynolds <josh.reynolds@frontier.com>
2025-08-20 14:49:02 -0700

    Converting Homer burger algorithm to dynamic programming approach

--- AT/03/burgers.c
+++ AT/03/burgers.c
@@ -4,8 +4,6 @@
 
 #define SIZE 10000
 
-unsigned long long total_calls;
-
 int max(int v1, int v2){
 	if (v1 > v2){
 		return v1;
@@ -14,63 +12,42 @@ int max(int v1, int v2){
 	}
 }
 
-int solve_t(int m, int n, int t, int memo[]){
-	int first, second;
-	total_calls++;
-
-	if (memo[t] != -2){
-		return memo[t];
-	}
-
-	if (t == 0){
-		memo[t] = 0;
-		return memo[t];
-	}
-
-	if (t >= m){
-		first = solve_t(m, n, t - m, memo);
-	} else {
-		first = -1;
-	}
-
-	if (t >= n){
-		second = solve_t(m, n, t - n, memo);
-	} else {
-		second = -1;
-	}
-
-	if (first == -1 && second == -1){
-		memo[t] = -1;
-		return memo[t];
-	} else {
-		memo[t] = max(first, second) + 1;
-		return memo[t];
-	}
-}
-
 void solve(int m, int n, int t){
-	int result, i;
-	int memo[SIZE];
+	int result, i, first, second;
+	int dp[SIZE];
+	dp[0] = 0;
 
-	for (i = 0; i <= t; i++){
-		memo[i] = -2;
+	for (i = 1; i <= t; i++){
+		if (i >= m){
+			first = dp[i - m];
+		} else {
+			first = -1;
+		}
+		if (i >= n){
+			second = dp[i-n];
+		} else {
+			second = -1;
+		}
+		if (first == -1 && second == -1){
+			dp[i] = -1;
+		} else {
+			dp[i] = max(first, second) + 1;
+		}
 	}
 
-	total_calls = 0;
-	result = solve_t(m, n, t, memo);
-
+	result = dp[t];
 	if (result >= 0){
 		printf("%d\n", result);
+
 	} else {
 		i = t - 1;
-		result = solve_t(m, n, i, memo);
+		result = dp[i];
 		while (result == -1){
 			i--;
-			result = solve_t(m, n, i, memo);
+			result = dp[i];
 		}
 		printf("%d %d\n", result, t - i);
 	}
-	printf("Total calls to solve_t: %llu\n", total_calls);
 }
 
 int main(void){

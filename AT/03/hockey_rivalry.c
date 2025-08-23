ec9b3230fc29c761e2423a409bb256557d8e7fa4
josh-reynolds <josh.reynolds@frontier.com>
2025-08-23 13:45:09 -0700

    Converting Hockey Rivals algorithm to dynamic programming approach

--- AT/03/hockey_rivalry.c
+++ AT/03/hockey_rivalry.c
@@ -13,37 +13,39 @@ int max(int v1, int v2){
 }
 
 int solve (char outcome1[], char outcome2[], 
-		int goals1[], int goals2[], int i, int j,
-		int memo[SIZE + 1][SIZE + 1]){
+		int goals1[], int goals2[], int n){
+	int i, j;
 	int first, second, third, fourth;
-	if (memo[i][j] != -1){
-		return memo[i][j];
+	static int dp[SIZE + 1][SIZE + 1];
+	for (i = 0; i <= n; i++){
+		dp[0][i] = 0;
 	}
-	if (i == 0 || j == 0){
-		memo[i][j] = 0;
-		return memo[i][j];
+	for (i = 0; i <= n; i++){
+		dp[i][0] = 0;
 	}
-	if ((outcome1[i] == 'W' && outcome2[j] == 'L' &&
-				goals1[i] > goals2[j]) ||
-	    (outcome1[i] == 'L' && outcome2[j] == 'W' &&
-				goals1[i] < goals2[j])){
-		first = solve(outcome1, outcome2, goals1, goals2, i - 1, j - 1, memo) +
-			goals1[i] + goals2[j];
-	} else {
-		first = 0;
+	for (i = 1; i <= n; i++){
+		for (j = 1; j <= n; j++){
+			if ((outcome1[i] == 'W' && outcome2[j] == 'L' &&
+						goals1[i] > goals2[j]) ||
+			    (outcome1[i] == 'L' && outcome2[j] == 'W' &&
+						goals1[i] < goals2[j])){
+				first = dp[i-1][j-1] + goals1[i] + goals2[j];
+			} else {
+				first = 0;
+			}
+			second = dp[i-1][j-1];
+			third = dp[i-1][j];
+			fourth = dp[i][j-1];
+			dp[i][j] = max(first, max(second, max(third, fourth)));
+		}
 	}
-	second = solve(outcome1, outcome2, goals1, goals2, i - 1, j - 1, memo);
-	third = solve(outcome1, outcome2, goals1, goals2, i - 1, j, memo);
-	fourth = solve(outcome1, outcome2, goals1, goals2, i, j - 1, memo);
-	memo[i][j] = max(first, max(second, max(third, fourth)));
-	return memo[i][j];
+	return dp[n][n];
 }
 
 int main(void){
 	int i, j, n, result;
 	char outcome1[SIZE + 1], outcome2[SIZE + 1];
 	int goals1[SIZE + 1], goals2[SIZE + 2];
-	static int memo[SIZE + 1][SIZE + 1];
 	scanf("%d ", &n);
 	for (i = 1; i <= n; i++){
 		scanf("%c", &outcome1[i]);
@@ -57,12 +59,7 @@ int main(void){
 	for (i = 1; i <= n; i++){
 		scanf("%d ", &goals2[i]);
 	}
-	for (i = 0; i < SIZE; i++){
-		for (j = 0; j < SIZE; j++){
-			memo[i][j] = -1;
-		}
-	}
-	result = solve(outcome1, outcome2, goals1, goals2, n, n, memo);
+	result = solve(outcome1, outcome2, goals1, goals2, n);
 	printf("%d\n", result);
 	return 0;
 }

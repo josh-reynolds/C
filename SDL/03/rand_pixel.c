d430df291cd2cc4ab8a338c189e2c7dad9eacc51
josh-reynolds <josh.reynolds@frontier.com>
2026-01-03 18:11:49 -0800

    Moving most globals to locals

--- SDL/03/rand_pixel.c
+++ SDL/03/rand_pixel.c
@@ -7,14 +7,10 @@ const int SCREEN_HEIGHT=480;
 
 SDL_Surface* g_pDisplaySurface = NULL;
 SDL_Event g_Event;
-SDL_Rect g_Rect;
-Uint8 g_Red, g_Green, g_Blue;
-Uint32 g_x, g_y;
-Uint32 g_Color;
 
 void set_pixel(char* pixels, Uint32* color){
 	if (SDL_MUSTLOCK(g_pDisplaySurface)) SDL_LockSurface(g_pDisplaySurface);
-	memcpy(pixels, &g_Color, g_pDisplaySurface->format->BytesPerPixel);
+	memcpy(pixels, color, g_pDisplaySurface->format->BytesPerPixel);
 	if (SDL_MUSTLOCK(g_pDisplaySurface)) SDL_UnlockSurface(g_pDisplaySurface);
 }
 
@@ -30,25 +26,22 @@ int main(int argc, char* argv[]){
 	if (SDL_Init(SDL_INIT_VIDEO) == -1){
 		fprintf(stderr, "Could not initialize SDL!\n");
 		exit(1);
-	} else {
-		fprintf(stdout, "SDL initialized properly!\n");
-		atexit(SDL_Quit);
 	}
 
 	g_pDisplaySurface = SDL_SetVideoMode(SCREEN_WIDTH, SCREEN_HEIGHT, 0, SDL_ANYFORMAT);
 
 	for (;;){
 		if (SDL_PollEvent(&g_Event) == 0){
-			g_Red = rand()%256;
-			g_Green = rand()%256;
-			g_Blue = rand()%256;
-			g_Color = SDL_MapRGB(g_pDisplaySurface->format, 
-					     g_Red, g_Green, g_Blue);
+			Uint8 red = rand()%256;
+			Uint8 green = rand()%256;
+			Uint8 blue = rand()%256;
+			Uint32 color = SDL_MapRGB(g_pDisplaySurface->format, 
+					          red, green, blue);
 
-			g_x = rand()%SCREEN_WIDTH;
-			g_y = rand()%SCREEN_HEIGHT;
+			Uint32 x = rand()%SCREEN_WIDTH;
+			Uint32 y = rand()%SCREEN_HEIGHT;
 
-			set_pixel(get_pixel_data(g_x, g_y), &g_Color);
+			set_pixel(get_pixel_data(x, y), &color);
 
 			SDL_UpdateRect(g_pDisplaySurface, 0, 0, 0, 0);
 		} else {

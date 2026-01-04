bbb5d976afc0deb10c3d431a3ee5967b7ee04906
josh-reynolds <josh.reynolds@frontier.com>
2026-01-04 15:44:22 -0800

    Starting setup for game loop demonstration

---
+++ SDL/pikuma/loop/src/main.c
@@ -0,0 +1,12 @@
+// simple game loop project from YouTube video by pikuma:
+// "Creating a Game Loop with C & SDL (Tutorial)"
+// ======================================================
+
+#include <stdio.h>
+
+int main(){
+	printf("Game is running...\n");
+	return 0;
+}
+
+

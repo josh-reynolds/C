5a5f24225c58f9bf77fbb1c49d96a9c562ec99ee
josh-reynolds <josh.reynolds@frontier.com>
2026-01-04 16:02:27 -0800

    Initializing SDL2

--- SDL/pikuma/loop/src/main.c
+++ SDL/pikuma/loop/src/main.c
@@ -3,9 +3,19 @@
 // ======================================================
 
 #include <stdio.h>
+#include <SDL2/SDL.h>
+#include "./constants.h"
+
+void initialize_window(void){
+	if (SDL_Init(SDL_INIT_EVERYTHING) != 0){
+		fprintf(stderr, "Error initializing SDL.\n");
+		exit(1);    // tutorial returns FALSE here instead of exiting
+	}
+	// tutorial returns TRUE on success
+}
 
 int main(){
-	printf("Game is running...\n");
+	initialize_window();
 	return 0;
 }
 
